b349cc894d5d8f85c6e04e360ea559321fa289f2
Alberto Quesada <albertoqv19@gmail.com>
2023-09-27 16:42:19 +0200

    Monitor Class: Test Setters and Default Ctor

--- SummerCampManagement/src/app/MonitorTest.java
+++ SummerCampManagement/src/app/MonitorTest.java
@@ -27,6 +27,40 @@ class MonitorTest {
         assertEquals(specialEducator, monitor.isSpecialEducator());
 	}
 	
+	@Test
+	void testDefaultConstructor() {
+		int id = 0;
+		String firstName = "";
+		String lastName = "";
+		boolean specialEducator = false;
+		
+		Monitor monitor = new Monitor();
+		
+		assertEquals(id, monitor.getId());
+        assertEquals(firstName, monitor.getFirstName());
+        assertEquals(lastName, monitor.getLastName());
+        assertEquals(specialEducator, monitor.isSpecialEducator());
+	}
+	
+	
+	@Test
+	void testSetters() {
+		Monitor monitor = new Monitor();
+		int id = 1;
+		String firstName = "Alberto";
+		String lastName = "Quesada";
+		boolean specialEducator = true;
+		
+		monitor.setId(id);
+		monitor.setFirstName(firstName);
+		monitor.setLastName(lastName);
+		monitor.setSpecialEducator(specialEducator);
+		
+		assertEquals(id, monitor.getId());
+        assertEquals(firstName, monitor.getFirstName());
+        assertEquals(lastName, monitor.getLastName());
+        assertEquals(specialEducator, monitor.isSpecialEducator());
+	}
 	
 	
 
