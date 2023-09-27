99d1fdc0d7a95ba3143a562a32221026eb1018ac
Alberto Quesada <albertoqv19@gmail.com>
2023-09-27 16:06:03 +0200

    Constructors Of Monitor Class

--- SummerCampManagement/src/app/Monitor.java
+++ SummerCampManagement/src/app/Monitor.java
@@ -5,4 +5,21 @@ public class Monitor {
 	private String firstName;
 	private String lastName;
 	private boolean SpecialEducator;
+	
+	
+	public Monitor() {
+		this.id= 0;
+		this.firstName = "";
+		this.lastName = "";
+		this.SpecialEducator = false;
+	}
+	
+	public Monitor(int id, String firstName, String lastName, boolean SpecialEducator) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.SpecialEducator = SpecialEducator;
+		
+	}
 }
+
