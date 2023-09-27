56078b55eeaa0597eecb4f743fd7773fb47fb7a7
José Andrés Trujillo <trujillohenaresj@gmail.com>
2023-09-27 12:05:08 +0200

    create class Assistant

---
+++ SummerCampManagement/src/app/Assistant.java
@@ -0,0 +1,36 @@
+package app;
+
+import java.util.Date;
+
+public class Assistant {
+
+	public Assistant(int id, String firstName, String lastName, Date birthDate, boolean requireSpecialAttention) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getLastName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getBirthDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object isRequireSpecialAttention() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
