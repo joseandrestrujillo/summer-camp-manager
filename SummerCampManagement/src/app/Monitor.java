93814376df54f413ea272e8d5801a21f1c36fd8e
Alberto Quesada <albertoqv19@gmail.com>
2023-09-27 16:16:28 +0200

    Monitor Class: Getters and Setters

--- SummerCampManagement/src/app/Monitor.java
+++ SummerCampManagement/src/app/Monitor.java
@@ -21,5 +21,42 @@ public class Monitor {
 		this.SpecialEducator = SpecialEducator;
 		
 	}
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public Object getFirstName() {
+		return this.firstName;
+	}
+	
+	public Object getLastName() {
+		return this.lastName;
+	}
+	
+	public Object isSpecialEducator() {
+		return this.SpecialEducator;
+	}
+	
+	public void setId(int id2) {
+		this.id = id2;
+	}
+	
+	public void setFirstName(String firstName2) {
+		this.firstName = firstName2;
+	}
+	
+	public void setLastName(String lastName2) {
+		this.lastName = lastName2;
+	}
+	
+	public void setSpecialEducator(boolean SpecialEducator2) {
+		this.SpecialEducator = SpecialEducator2;
+		
+	}
+	
+	
 }
+
+	
 
