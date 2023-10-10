0806a2143421201b76a0711c6d93a3d6c24b079a
Alberto Quesada <albertoqv19@gmail.com>
2023-10-10 10:04:15 +0200

    Create entity NotFound exceptions

---
+++ SummerCampManagement/src/domain/exceptions/ActivityNotFoundException.java
@@ -0,0 +1,7 @@
+package domain.exceptions;
+
+public class ActivityNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ SummerCampManagement/src/domain/exceptions/AssistantNotFoundException.java
@@ -0,0 +1,7 @@
+package domain.exceptions;
+
+public class AssistantNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
