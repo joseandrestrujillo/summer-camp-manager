663378dd6d65ec5cbf1da48b12f741d967b50bf2
GitRadiation <prietoreyesalvaro@gmail.com>
2023-10-10 13:20:00 +0200

    AssistantsManager + AssistantsManagerTests

--- SummerCampManagement/src/managers/AssistantsManager.java
+++ SummerCampManagement/src/managers/AssistantsManager.java
@@ -1,5 +1,25 @@
 package managers;
 
+import domain.entities.Assistant;
+import domain.exceptions.NotFoundException;
+import domain.interfaces.IRepository;
+import repositories.InMemoryAssistantRepository;
+
 public class AssistantsManager {
+	private IRepository<Assistant> assistantRepository;
+	public AssistantsManager(IRepository<Assistant> assistantRepository) {
+		this.assistantRepository = assistantRepository;
+	}
+	public void registerAssistant(Assistant assistant) {
+		
+	}
+	public boolean isRegistered(Assistant assistant) {
+		try {
+			this.assistantRepository.find(assistant.getId());
+			return true;
+		} catch (NotFoundException e) {
+			return false;
+		}
+	}
 
 }
---
+++ SummerCampManagement/tests/managers/AssistantsManagerTest.java
@@ -0,0 +1,31 @@
+package managers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import domain.entities.Assistant;
+import repositories.InMemoryAssistantRepository;
+import utilities.Utils;
+
+class AssistantsManagerTest {
+
+	@Test
+	void registerAssistant_whenAssistantIsNotRegistered_thenRegisterTheAssistant(){
+		InMemoryAssistantRepository assistantRepository = new InMemoryAssistantRepository();
+		
+		Assistant assistant = new Assistant(
+				1,
+				"José",
+				"Trujillo",
+				Utils.parseDate("26/01/2001"),
+				true
+				);
+		
+		AssistantsManager assistantsManager = new AssistantsManager(assistantRepository);
+		assistantsManager.registerAssistant(assistant);
+		assertEquals(true, assistantsManager.isRegistered(assistant));
+	}
+	
+	
+}
