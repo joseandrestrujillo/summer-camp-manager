fd2089d3ff4d2d91e71048d755aec94be30199a6
GitRadiation <prietoreyesalvaro@gmail.com>
2023-10-12 12:57:27 +0200

    Refactor enroll : create function

--- SummerCampManagement/src/managers/InscriptionManager.java
+++ SummerCampManagement/src/managers/InscriptionManager.java
@@ -13,6 +13,7 @@ import domain.exceptions.AssistantAlreadyEnrolledException;
 import domain.exceptions.NotFoundException;
 import domain.factories.EarlyRegisterInscriptionFactory;
 import domain.factories.LateRegisterInscriptionFactory;
+import domain.interfaces.AbstractInscriptionFactory;
 import domain.interfaces.IRepository;
 
 public class InscriptionManager {
@@ -29,7 +30,21 @@ public class InscriptionManager {
 		this.assitantRepository = assitantRepository;
 		this.inscriptionRepository = inscriptionRepository;
 	}
-	
+	private Inscription create(
+			AbstractInscriptionFactory factory,
+			int assistantId, 
+			int campId, 
+			Date inscriptionDate, 
+			boolean isPartial, 
+			float price ) {
+		Inscription inscription;
+		if (! isPartial) {
+			inscription = factory.createComplete(assistantId, campId, inscriptionDate, price);
+		} else {
+			inscription = factory.createPartial(assistantId, campId, inscriptionDate, price);
+		}
+		return inscription;
+	}
 	public Inscription enroll(
 			int assistantId, 
 			int campId, 
@@ -39,6 +54,8 @@ public class InscriptionManager {
 	) {
 		String inscriptionId = assistantId + "-" + campId;
 		int nActivities = campRepository.find(campId).getActivities().size();
+		float basePrice = isPartial ? 100 : 300;
+		float price = basePrice + 20*nActivities;
 		
 		try {
 			this.inscriptionRepository.find(inscriptionId);
@@ -49,18 +66,10 @@ public class InscriptionManager {
 		
 		try {
 			EarlyRegisterInscriptionFactory factory = new EarlyRegisterInscriptionFactory(campRepository, assitantRepository);
-			if (! isPartial) {
-				inscription = factory.createComplete(assistantId, campId, inscriptionDate, 300+20*nActivities);
-			} else {
-				inscription = factory.createPartial(assistantId, campId, inscriptionDate, 100+20*nActivities);
-			}
+			inscription = create(factory, assistantId, campId, inscriptionDate, isPartial, price);
 		} catch (AfterEarlyTimeException e) {
 			LateRegisterInscriptionFactory factory = new LateRegisterInscriptionFactory(campRepository, assitantRepository);
-			if (! isPartial) {
-				inscription = factory.createComplete(assistantId, campId, inscriptionDate, 300+20*nActivities);
-			} else {
-				inscription = factory.createPartial(assistantId, campId, inscriptionDate, 100+20*nActivities);
-			}
+			inscription = create(factory, assistantId, campId, inscriptionDate, isPartial, price);
 		}
 		
 		this.inscriptionRepository.save(inscription);
