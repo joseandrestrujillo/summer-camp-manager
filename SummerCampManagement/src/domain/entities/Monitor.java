33d170385c824d23bab6218aa0cde13b57727d4f
GitRadiation <prietoreyesalvaro@gmail.com>
2023-10-14 11:58:49 +0200

    Monitor y PartialInscription : Javadoc

--- SummerCampManagement/src/domain/entities/Monitor.java
+++ SummerCampManagement/src/domain/entities/Monitor.java
@@ -1,72 +1,120 @@
 package domain.entities;
-
+/**
+ * La clase Monitor representa a un monitor en un sistema educativo.
+ */
 public class Monitor {
-	private int id;
-	private String firstName;
-	private String lastName;
-	private boolean specialEducator;
-	
-	
-	public Monitor() {
-		this.id= 0;
-		this.firstName = "";
-		this.lastName = "";
-		this.specialEducator = false;
-	}
-	
-	public Monitor(int id, String firstName, String lastName, boolean SpecialEducator) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.specialEducator = SpecialEducator;
-		
-	}
-	
-	public Integer getId() {
-		return this.id;
-	}
-	
-	public Object getFirstName() {
-		return this.firstName;
-	}
-	
-	public Object getLastName() {
-		return this.lastName;
-	}
-	
-	public Object isSpecialEducator() {
-		return this.specialEducator;
-	}
-	
-	public void setId(int id2) {
-		this.id = id2;
-	}
-	
-	public void setFirstName(String firstName2) {
-		this.firstName = firstName2;
-	}
-	
-	public void setLastName(String lastName2) {
-		this.lastName = lastName2;
-	}
-	
-	public void setSpecialEducator(boolean SpecialEducator2) {
-		this.specialEducator = SpecialEducator2;
-		
-	}
-	
-	public String toString() {
-		return "{id: " 
-				+ this.id 
-				+ ", firstName: '"
-				+ this.firstName
-				+ "', lastName: '"
-				+ this.lastName 
-				+ "', isSpecialEducator: "
-				+ this.specialEducator + "}";
-		
-	}
-		
+    private int id;             // El identificador del monitor
+    private String firstName;   // El primer nombre del monitor
+    private String lastName;    // El apellido del monitor
+    private boolean specialEducator; // Indica si el monitor es educador especial
+
+    /**
+     * Constructor predeterminado de la clase Monitor.
+     * Inicializa los campos con valores predeterminados.
+     */
+    public Monitor() {
+        this.id = 0;
+        this.firstName = "";
+        this.lastName = "";
+        this.specialEducator = false;
+    }
+
+    /**
+     * Constructor de la clase Monitor que toma parámetros.
+     *
+     * @param id El identificador del monitor.
+     * @param firstName El primer nombre del monitor.
+     * @param lastName El apellido del monitor.
+     * @param SpecialEducator Indica si el monitor es educador especial.
+     */
+    public Monitor(int id, String firstName, String lastName, boolean SpecialEducator) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.specialEducator = SpecialEducator;
+    }
+
+    /**
+     * Obtiene el identificador del monitor.
+     *
+     * @return El identificador del monitor.
+     */
+    public Integer getId() {
+        return this.id;
+    }
+
+    /**
+     * Obtiene el primer nombre del monitor.
+     *
+     * @return El primer nombre del monitor.
+     */
+    public Object getFirstName() {
+        return this.firstName;
+    }
+
+    /**
+     * Obtiene el apellido del monitor.
+     *
+     * @return El apellido del monitor.
+     */
+    public Object getLastName() {
+        return this.lastName;
+    }
+
+    /**
+     * Comprueba si el monitor es un educador especial.
+     *
+     * @return `true` si el monitor es un educador especial, `false` en caso contrario.
+     */
+    public Object isSpecialEducator() {
+        return this.specialEducator;
+    }
+
+    /**
+     * Establece el identificador del monitor.
+     *
+     * @param id2 El nuevo identificador del monitor.
+     */
+    public void setId(int id2) {
+        this.id = id2;
+    }
+
+    /**
+     * Establece el primer nombre del monitor.
+     *
+     * @param firstName2 El nuevo primer nombre del monitor.
+     */
+    public void setFirstName(String firstName2) {
+        this.firstName = firstName2;
+    }
+
+    /**
+     * Establece el apellido del monitor.
+     *
+     * @param lastName2 El nuevo apellido del monitor.
+     */
+    public void setLastName(String lastName2) {
+        this.lastName = lastName2;
+    }
+
+    /**
+     * Establece si el monitor es un educador especial.
+     *
+     * @param SpecialEducator2 `true` si el monitor es un educador especial, `false` en caso contrario.
+     */
+    public void setSpecialEducator(boolean SpecialEducator2) {
+        this.specialEducator = SpecialEducator2;
+    }
+
+    /**
+     * Devuelve una representación en cadena del monitor en formato JSON.
+     *
+     * @return Una cadena que representa al monitor en formato JSON.
+     */
+    public String toString() {
+        return "{id: " + this.id + ", firstName: '" + this.firstName + "', lastName: '" + this.lastName
+                + "', isSpecialEducator: " + this.specialEducator + "}";
+    }
 }
 
 	
--- SummerCampManagement/src/domain/entities/PartialInscription.java
+++ SummerCampManagement/src/domain/entities/PartialInscription.java
@@ -2,10 +2,23 @@ package domain.entities;
 
 import java.util.Date;
 
-public class PartialInscription extends Inscription{
+/**
+ * Clase que representa una inscripción parcial a un campamento.
+ * Esta clase hereda de la clase Inscription y proporciona
+ * información específica sobre inscripciones parciales.
+ */
+public class PartialInscription extends Inscription {
 
-	public PartialInscription(int assistantId, int campId, Date inscriptionDate, float price, boolean canBeCanceled) {
-		super(assistantId, campId, inscriptionDate, price, canBeCanceled);
-	}
-
+    /**
+     * Constructor de la clase PartialInscription.
+     *
+     * @param assistantId     ID del asistente que se inscribe.
+     * @param campId          ID del campamento al que se inscribe.
+     * @param inscriptionDate Fecha de la inscripción.
+     * @param price           Precio de la inscripción parcial.
+     * @param canBeCanceled   Indica si la inscripción parcial puede ser cancelada.
+     */
+    public PartialInscription(int assistantId, int campId, Date inscriptionDate, float price, boolean canBeCanceled) {
+        super(assistantId, campId, inscriptionDate, price, canBeCanceled);
+    }
 }
